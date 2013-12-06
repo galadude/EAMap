beece121d79ce2e9325ef9a75a4994e3b2c2af70
Michael Galanakis <michael@galanakis.dk>
2013-12-06 21:06:47 +0100

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Eamap::Application.config.secret_key_base = '5c0f0d5c34685e521df72429786548686dd9667d790741c982bdac1736cd9227f494f460f2f4b5143c299cda9f7b2d21092b15b4e92751329ffdeac33b844791'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Eamap::Application.config.session_store :cookie_store, key: '_eamap_session'
