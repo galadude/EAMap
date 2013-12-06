7f03b63920475a04855396626bb9fdf25dcab096
Michael Galanakis <michael@galanakis.dk>
2013-12-06 23:17:20 +0100

    events form is getting there

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,6 +2,10 @@ class EventsController < ApplicationController
 	def new
 	end
 
+	def show
+		@event = Event.find(params[:id])
+	end
+
 	def create 
 		@event = Event.new(params.require(:event).permit(
 		:name, :location, :description, :organisation, :latitude, :longitude
